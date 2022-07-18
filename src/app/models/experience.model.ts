6304830c347695e827b1cfc2b994f6af2c32a281
Adriana Coronel <adriana.coronel@plerk.io>
2022-07-17 23:24:23 -0300

    ✨experience model

---
+++ src/app/models/experience.model.ts
@@ -0,0 +1,10 @@
+export interface Experience {
+  startDate: string;
+  endDate: string;
+  company: string;
+  title: string;
+  description: string;
+  type: string;
+  location: string;
+  id: number;
+}
